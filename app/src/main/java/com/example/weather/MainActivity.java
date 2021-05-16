525f388713ed7c3d874aa6d3afacf4bd4d73b734
systemry420 <systemry420@gmail.com>
2021-05-16 18:25:16 +0300

    Initial commit: build the layout

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+    final long MIN_TIME = 5000;
+    final float MIN_DISTANCE = 1000;
+
+    // TODO: Set LOCATION_PROVIDER here:
+
+
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
+
+    // TODO: Declare a LocationManager and a LocationListener here:
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_controller_layout);
+
+        mCityLabel = findViewById(R.id.locationTV);
+        mWeatherImage = findViewById(R.id.weatherSymbolIV);
+        mTemperatureLabel = findViewById(R.id.tempTV);
+        ImageButton changeCityButton = findViewById(R.id.changeCityButton);
+
+
+
+        // TODO: Add an OnClickListener to the changeCityButton here:
+
+    }
+
+
+    // TODO: Add onResume() here:
+
+
+
+    // TODO: Add getWeatherForNewCity(String city) here:
+
+
+
+    // TODO: Add getWeatherForCurrentLocation() here:
+
+
+
+    // TODO: Add letsDoSomeNetworking(RequestParams params) here:
+
+
+
+    // TODO: Add updateUI() here:
+
+
+
+    // TODO: Add onPause() here:
+
+
+
+}
